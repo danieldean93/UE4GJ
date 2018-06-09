9c6f934f87d4a88322831850ac4bdbe5287f371c
DESKTOP-KVJNAPG\toddm <toddmoulder88@gmail.com>
2018-06-09 13:02:22 -0500

    STUFF.  no more stuff in level bp.  Trigger boxes are bps now.  Classes are using interfaces to talk to one another.

---
+++ Source/Music_Controller/Music_Controller.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Music_Controller.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Music_Controller, "Music_Controller" );
